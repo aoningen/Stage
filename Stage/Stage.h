b6be2011c8f9a50df7f5eb09a6ffc268849be2a3
zwei-i5 <q94zdmqtd7@privaterelay.appleid.com>
2026-08-25 18:06:27 +0900

    main

---
+++ Stage/Stage.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "DxLib.h"
+
+class Stage
+{
+public:
+	//コンストラクタとデストラクタ
+	Stage();
+	~Stage();
+	//ステージを読みこむ
+	bool Load();
+
+	//ステージの位置を設定
+	void SetPosition(VECTOR pos);
+
+	//ステージを描画
+	void Draw();
+
+private:
+	//ステージの3Dモデル
+	int modelHandle;
+
+	//ステージの位置
+	VECTOR position;
+};
+
+Stage::Stage()
+{
+}
+
+Stage::~Stage()
+{
+}
--- Stage/main.cpp
+++ Stage/main.cpp
@@ -1,1 +1,58 @@
 #include"DxLib.h"
+#include"Stage.h"
+
+const int WINDOW_WDITH = 1280;
+const int WINDOW_HEIGHT = 720;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//ウィンドウモード
+	ChangeWindowMode(TRUE);
+	//画面サイズ
+	SetGraphMode(WINDOW_WDITH, WINDOW_HEIGHT, 32);
+
+	//DxLib初期化
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	//裏画面	
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	//Stageオブジェクトを作成
+
+	Stage stage;
+
+	//ステージ読み込み
+	if (!stage.Load())
+	{
+		DxLib_End();
+		return -1;
+	}
+
+	//カメラ
+	VECTOR cameraPos = VGet(0.0f, 10.0f, 1100.0f);
+	VECTOR cameraTarget = VGet(0.0f, 0.0f, 0.0f);
+
+	SetCameraPositionAndTarget_UpVecY(cameraPos, cameraTarget);
+
+	//ゲームループ
+
+	while (ProcessMessage()==0&&CheckHitKey(KEY_INPUT_ESCAPE)==0)
+	{
+		//画面クリア
+		ClearDrawScreen();
+
+		//ステージ描画
+		stage.Draw();
+
+		//裏画面を表画面へ
+		ScreenFlip();
+	}
+
+	//Dxlib終了
+	DxLib_End();
+
+	return 0;
+}
