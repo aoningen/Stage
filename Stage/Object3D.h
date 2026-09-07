842c6c9cb7633f2753a6a1db320b2e6f4f8f8bae
aoningen <aokitune0125@gmail.com>
2026-09-07 14:38:20 +0900

    Object3DとPlayerのヘッダーを追加

---
+++ Stage/Object3D.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "DxLib.h"
+class Object3D
+{
+public:
+	Object3D();
+	~Object3D();
+
+	bool Load(const char*fileName,float radius);
+	void SetPosition(VECTOR pos);
+	VECTOR GetPositon() const;
+	float GetCollisionRadius() const;
+
+	void Draw();
+
+private:
+	int modelHandle;
+	VECTOR position;
+	float collisionRadius;
+
+};
---
+++ Stage/Player.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "DxLib.h"
+class Player
+{
+private:
+	int modelHandle;
+	VECTOR position;
+	float moveSpeed;
+	float collisionRadius;
+public:
+	Player();
+	~Player();
+
+	bool Load();
+	void Update();
+	void Draw();
+	VECTOR GetPosition() const;
+	float GetCollisionRadius() const;
+	void SetPosition(VECTOR pos);
+};
